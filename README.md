20087a0bcf917eefdcae61159920e985e94f9749
Joel Jönsson <85joel@gmail.com>
2017-09-06 12:27:40 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Simple Redux example
+Made with guidance from the udemy.com React course ["Modern React with Redux"](https://www.udemy.com/react-redux/)
+
+```
+> npm install
+> npm start
+```
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        { title: 'Javascript: The goofy parts', pages: 101},
+        { title: 'Harry Poucher', pages: 240},
+        { title: 'The Bark Tower', pages: 230},
+        { title: 'C++ plus minus one', pages: 100},
+    ]
+}
